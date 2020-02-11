4e52595388e5ccf6f3f4ef96a68f3bc1ef794a5f
unknown <neubauersim@gmail.com>
2020-02-11 22:22:23 +0100

    update

--- performance-dashboard/index.js
+++ performance-dashboard/index.js
@@ -1,41 +1,38 @@
-console.log('Performance Dashboard startet...')
 const os = require('os');
 const utils = require('os-utils');
-var express = require('express');
-var app = express();
+const WebSocket = require('ws')
+const wss = new WebSocket.Server({ port: 5000 })
+
 const data = {};
+const cpu = '0';
 
-console.log('OS Typ: ',os.type())
-console.log('CPU Architektur: ',os.arch)
-console.log('OS Plattform: ',os.platform())
-console.log('OS Release: ',os.release())
+console.log('OS Typ: ', os.type())
+console.log('CPU Architektur: ', os.arch)
+console.log('OS Plattform: ', os.platform())
+console.log('OS Release: ', os.release())
 
-data.os = os.type();
-data.arch = os.arch();
-data.platform = os.platform();
-data.release = os.release();
+function setData() {
+  data.os = os.type();
+  data.arch = os.arch();
+  data.platform = os.platform();
+  data.release = os.release();
+  utils.cpuUsage((v) => 
+    data.cpu = (v * 100).toString().substr(0, 5));
+}
 
+function getData() {
+  return data;
+}
 
-/*
-setInterval(() => {
-utils.cpuUsage(function(v){
-    console.log(new Date())
-    console.log( 'CPU Usage (%): ' + (v *100).toString().substr(0, 5));
-});
-}, 10);
-*/
+wss.on('connection', ws => {
+  ws.on('message', message => {
+    console.log(`Received message => ${message}`)
+  })
+  setInterval(() => {
+    setData();
+    ws.send(JSON.stringify(getData()))
+  }, 100);
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+  
+})
 
-
-app.get('/', function (req, res) {
-  res.json(data);
-});
-
-app.listen(5000, function () {
-  console.log('Example app listening on port 5000!');
-});
