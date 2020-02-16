2ce8df41921d0acd8fb73bc067f75efdfee0216a
unknown <neubauersim@gmail.com>
2020-02-16 18:25:04 +0100

    update

--- performance-dashboard/index.js
+++ performance-dashboard/index.js
@@ -2,7 +2,8 @@ const os = require('os');
 const utils = require('os-utils');
 const WebSocket = require('ws')
 
-const wss = new WebSocket.Server({ port: 5000 });
+const wss = new WebSocket.Server({ host: '10.0.0.2', port: 5000 });
+//const wss = new WebSocket({host:'ws://localhost', port:5000});
 
 const data = {};
 
@@ -33,6 +34,7 @@ wss.on('connection', ws => {
     ws.send(JSON.stringify(getData()))
   }, 100);
 
+  
   
 })
 
